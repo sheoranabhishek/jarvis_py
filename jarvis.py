c16bbfb5df48d13077d1b5e47bf83a3e1d96ff7f
myusername <myemail>
2020-06-25 20:53:11 +0530

    first commit

--- jarvis.py
+++ jarvis.py
@@ -3,6 +3,7 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
+import os
 
 engine = pyttsx3.init('sapi5')  # microsoft speech api
 voices = engine.getProperty('voices')
@@ -26,11 +27,11 @@ def wishMe():
     hour = int(datetime.datetime.now().hour)
     # print(hour)
     if hour >= 0 and hour <= 12:
-        speak("Subh ki Ram Ram , Hello Sheoran , How May I Help You ?")
+        speak(" Hey ! Good Morning Jarvis.")
     elif hour >= 12 and hour < 18:
-        speak("Dophr ki Ram Ram , Hello Sheoran , How May I Help You ?")
+        speak("Hey ! Good Afternoon Jarvis.")
     else:
-        speak("Sham ki Ram Ram , Hello Sheoran , How May I Help You ?")
+        speak("Hey ! Good Evening Jarvis.")
 
 
 def takeCommand():
@@ -62,6 +63,9 @@ if __name__ == "__main__":
     while True:
         query = takeCommand().lower()
 
+        if 'good morning' in query:
+            speak("Hey! Good Morning Abhishek . What's my task ?")
+
         if 'wikipedia' in query:
             speak('Searching Wikipedia ... ')
             query = query.replace("wikipedia", "")
@@ -69,5 +73,18 @@ if __name__ == "__main__":
             speak("According to wikipedia")
             speak(results)
 
-        elif '.com' in query:
+        elif 'youtube.com' in query:
             webbrowser.get(chrome_path).open('youtube.com')
+
+        elif 'stackoverflow' in query:
+            webbrowser.get(chrome_path).open('stackoverflow.com')
+
+        elif 'play music' in query:
+            music_dir = "C:\\Users\\LENOVO\\Desktop"
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f'Sir , the time is : {strTime} ')
